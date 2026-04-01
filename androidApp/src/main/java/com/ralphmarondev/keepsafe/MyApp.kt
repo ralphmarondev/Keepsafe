c8f589fe9c4e07701638a9ba4e4bcf979f0ab103
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-04-01 17:16:48 +0800

    remove unused import

--- androidApp/src/main/java/com/ralphmarondev/keepsafe/MyApp.kt
+++ androidApp/src/main/java/com/ralphmarondev/keepsafe/MyApp.kt
@@ -2,8 +2,6 @@ package com.ralphmarondev.keepsafe
 
 import android.app.Application
 import com.ralphmarondev.keepsafe.di.initKoin
-import dev.gitlive.firebase.Firebase
-import dev.gitlive.firebase.initialize
 import org.koin.android.ext.koin.androidContext
 
 class MyApp : Application() {
