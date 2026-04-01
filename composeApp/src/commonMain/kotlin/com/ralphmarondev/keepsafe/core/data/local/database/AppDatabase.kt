4dc5e0ad0c490432993f27d228eb5317740202d2
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-04-01 12:22:02 +0800

    fix incorrect type on AppDatabase

--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/data/local/database/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/data/local/database/AppDatabase.kt
@@ -2,6 +2,7 @@ package com.ralphmarondev.keepsafe.core.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ralphmarondev.keepsafe.core.data.local.database.dao.FamilyDao
 import com.ralphmarondev.keepsafe.core.data.local.database.dao.MemberDao
 import com.ralphmarondev.keepsafe.core.data.local.database.entity.FamilyEntity
 import com.ralphmarondev.keepsafe.core.data.local.database.entity.MemberEntity
@@ -14,7 +15,7 @@ import com.ralphmarondev.keepsafe.core.data.local.database.entity.MemberEntity
     version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
-    abstract val familyDao: FamilyEntity
+    abstract val familyDao: FamilyDao
     abstract val memberDao: MemberDao
 
     companion object {
