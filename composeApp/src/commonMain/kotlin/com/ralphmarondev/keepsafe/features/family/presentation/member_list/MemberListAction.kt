181ca30757a5675471b92d2134595ed4862e85bd
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-04-04 12:19:09 +0800

    implement load and show members on member list screen

--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/features/family/presentation/member_list/MemberListAction.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/features/family/presentation/member_list/MemberListAction.kt
@@ -3,4 +3,5 @@ package com.ralphmarondev.keepsafe.features.family.presentation.member_list
 sealed interface MemberListAction {
     data object AddNewMember : MemberListAction
     data object ClearNavigation : MemberListAction
+    data object Refresh : MemberListAction
 }
--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/features/family/presentation/member_list/MemberListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/features/family/presentation/member_list/MemberListViewModel.kt
@@ -1,19 +1,30 @@
 package com.ralphmarondev.keepsafe.features.family.presentation.member_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ralphmarondev.keepsafe.features.family.domain.repository.FamilyRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class MemberListViewModel : ViewModel() {
+class MemberListViewModel(
+    private val familyRepository: FamilyRepository
+) : ViewModel() {
 
     private val _state = MutableStateFlow(MemberListState())
     val state = _state.asStateFlow()
+
+    init {
+        loadAllMembers()
+    }
 
     fun onAction(action: MemberListAction) {
         when (action) {
             MemberListAction.AddNewMember -> addNewMember()
             MemberListAction.ClearNavigation -> clearNavigation()
+            MemberListAction.Refresh -> loadAllMembers(isRefreshing = true)
         }
     }
 
@@ -23,5 +34,36 @@ class MemberListViewModel : ViewModel() {
 
     private fun clearNavigation() {
         _state.update { it.copy(navigateToNewMember = false) }
+    }
+
+    private fun loadAllMembers(isRefreshing: Boolean = false) {
+        viewModelScope.launch {
+            try {
+                _state.update {
+                    it.copy(
+                        isRefreshing = isRefreshing,
+                        message = null,
+                        isError = false,
+                        showMessage = false
+                    )
+                }
+
+                val members = familyRepository.getAllMembers()
+                if (isRefreshing) {
+                    delay(1500)
+                }
+                _state.update { it.copy(members = members) }
+            } catch (e: Exception) {
+                _state.update {
+                    it.copy(
+                        message = e.message,
+                        isError = true,
+                        showMessage = true
+                    )
+                }
+            } finally {
+                _state.update { it.copy(isRefreshing = false) }
+            }
+        }
     }
 }
